bf147fedb71de7cd40b6f8854ede49ecc5d4714d
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-02-05 01:53:37 +0900

    add function of income

--- services/tweet.py
+++ services/tweet.py
@@ -3,7 +3,7 @@ import requests
 from requests_oauthlib import OAuth1Session
 
 class Tweet():
-    def tweet(self, conf):
+    def tweet(self, conf, message):
         self.twitter_consumer_key = conf.twitter_consumer_key
         self.twitter_consumer_secret = conf.twitter_consumer_secret
         self.twitter_access_token = conf.twitter_access_token
@@ -12,7 +12,7 @@ class Tweet():
         url = 'https://api.twitter.com/1.1/statuses/update.json'
 
         auth = OAuth1Session(self.twitter_consumer_key, self.twitter_consumer_secret, self.twitter_access_token, self.twitter_access_token_secret)
-        r = auth.post(url, params={"status":"Hello"})
+        r = auth.post(url, params={"status":message})
 
         if r.status_code == 200: #スターテスコードの確認
             print ("connect succeed!")
