c45e6a85018dd31b5438a2ad06f04ec09ef20c80
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-02-08 23:15:54 +0900

    add sending spotify api

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ import sys
 from argparse import ArgumentParser
 from configure import Configure
 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -11,7 +14,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction, CarouselTemplate, CarouselColumn
 )
 
 app = Flask(__name__)
@@ -19,6 +22,11 @@ app = Flask(__name__)
 conf = Configure()
 line_bot_api = LineBotApi(conf.channel_access_token)
 handler = WebhookHandler(conf.channel_secret)
+
+client_id = conf.spotify_client_id
+client_secret = conf.spotify_client_secret
+client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -39,10 +47,35 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+    playlists = get_playlists()
+    colums_list = [ CarouselColumn(
+        thumbnail_image_url=i['images'][0]['url'],
+        title=i['name'],
+        text='playlist',
+        actions=[
+            URITemplateAction(
+                label='Go',
+                uri=i['external_urls']['spotify']
+                )
+            ]
+        ) for i in playlists ]
+    print(colums_list)
+
+
+    carousel_template_message = TemplateSendMessage(
+	alt_text='Carousel template',
+	template=CarouselTemplate(
+	    columns=colums_list
+	)
+    )
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+	carousel_template_message
     )
+
+def get_playlists():
+    result = spotify.featured_playlists(limit=5)
+    return result['playlists']['items']
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
